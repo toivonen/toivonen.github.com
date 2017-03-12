3526fc6612d4637de25d8ff70fa8f679656556a0
Varya Stepanova <mail@varya.me>
2017-03-12 22:42:50 +0200

    publish: Some additional info
    generated from commit 56d367de38775161c1b8219c168e20ec3a9747ca

--- assets/loader-75517307ff8866ab904b.js
+++ assets/loader-75517307ff8866ab904b.js
@@ -80,7 +80,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "f9fd13ed5e4c44fedc8e" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "75517307ff8866ab904b" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
