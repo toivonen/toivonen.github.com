da410f608e3f651ad316db7f97f1824b7ed803a1
Varya Stepanova <mail@varya.me>
2017-07-18 15:50:33 +0300

    publish: Correct date
    generated from commit baad32a1abdf94c57670b64e31dfbb6f6f99b543

--- assets/loader-89beb965cd34684c6acf.js
+++ assets/loader-89beb965cd34684c6acf.js
@@ -90,7 +90,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "cd3214c8513a63ee56fb" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "89beb965cd34684c6acf" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
