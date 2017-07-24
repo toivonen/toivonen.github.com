686401111872b1099c2a8e91a08f03e73c2c56e0
Varya Stepanova <mail@varya.me>
2017-07-24 14:46:17 +0300

    publish: Insert picture into article
    generated from commit 69fc947e414666af4d0af4cd09d867820f54505e

--- assets/loader-5fdaeedbc3142600a4ad.js
+++ assets/loader-5fdaeedbc3142600a4ad.js
@@ -90,7 +90,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "f453f65a13dcfbbde40b" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "5fdaeedbc3142600a4ad" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
