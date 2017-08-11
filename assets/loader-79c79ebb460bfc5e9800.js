312d252c9fcfbf258c37a976c24f4bfa7414c671
Varya Stepanova <mail@varya.me>
2017-08-11 14:53:07 +0300

    publish: [post] Life in Finland, part 4
    generated from commit f896227653fb7bd1e21a33e7bef5c6dc4e720526

--- assets/loader-79c79ebb460bfc5e9800.js
+++ assets/loader-79c79ebb460bfc5e9800.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "941b18286582bc0f417d" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-" + "79c79ebb460bfc5e9800" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
